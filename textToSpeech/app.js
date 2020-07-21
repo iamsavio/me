517a06481f5e1b272bfac38d581a78b73d15a7c6
Savio <xwall256@gmail.com>
2020-07-22 00:57:58 +0300

    bible

--- textToSpeech/app.js
+++ textToSpeech/app.js
@@ -1,31 +1,79 @@
 
-var app = angular.module('myApp', ['voiceRss']);
-// app.config(['$routerProvider',function($routeProvider) {
-//     $routeProvider
-//     .when("/", {
-//       templateUrl : "main.html"
-//     })
-//     .when("/red", {
-//       templateUrl : "gallery.html"
-//     })
-//     .when("/green", {
-//       templateUrl : "about.html"
-//     }).otherwise({redirectTo:'/'})
+var app = angular.module('myApp', ['voiceRss','ngRoute']);
+app.config(['$routeProvider',function($routeProvider) {
+    $routeProvider
+    .when("/", {
+      templateUrl : "text.html"
+    })
+    .when("/UploadDoc", {
+      templateUrl : "upload.html"
+    })
+    .when("/Bible", {
+      templateUrl : "bible.html"
+    }).otherwise({redirectTo:'/'})
     
-// }]);
+}]);
 
 app.config(['ttsProvider', function (ttsProvider) {
     ttsProvider.setSettings({ key: '4a45337a5dfd410b93d8e40e0fceb10d' });
 }]);
 
-app.controller('ctrl', ['tts', '$scope',function (tts,$scope) {
+app.controller('ctrl', ['tts', '$scope','$http',function (tts,$scope,$http) {
+  
+var req = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://bible-references.p.rapidapi.com/api/passages?lang=en",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "bible-references.p.rapidapi.com",
+		"x-rapidapi-key": "9422a96803msh3cd21a2ec80bdaep183961jsnceb2210261c7"
+	}
+}
+$scope.books=[]
+$scope.passage=""
+$scope.chapter=""
+$scope.verse_start=""
+$scope.verse_end=""
+
+$http(req).then((data) => {
+      $scope.books=data.data.data['3'].passages
+    })
+
+ $scope.readVerse=()=>{
+ var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://bible-references.p.rapidapi.com/api/verses/kjv/"+$scope.passage+"/"+$scope.passage+"%20"+$scope.chapter+"?verse_start="+$scope.verse_start+"&verse_end="+$scope.verse_end,
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "bible-references.p.rapidapi.com",
+		"x-rapidapi-key": "9422a96803msh3cd21a2ec80bdaep183961jsnceb2210261c7"
+	}
+}
+
+$.ajax(settings).done(function (response) {
+  let newArray =[]
+  response.data[0].verse.map((txt)=>{
+   let arr = txt.text.replace(/[&\/\\#+()$~%'":*<>{}]/g, '')
+    newArray.push(arr)
+  })
+
+  $scope.txtToSpeech= newArray.toString()
+  $scope.readTxt()
+
+});
+}
+
+
+  //read text
   $scope.txtToSpeech=""
 $scope.readTxt=()=>{
   if($scope.txtToSpeech){
      tts.speech({
         src: $scope.txtToSpeech,
         hl: 'en-us',
-        r: 0, 
+        r: -3, 
         c: 'mp3',
         f: '44khz_16bit_stereo',
         ssml: false
